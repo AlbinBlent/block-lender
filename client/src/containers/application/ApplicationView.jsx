18b36e99218ec1c6b1ccb1e75798feebf93f95a1
Matilda Dahlqvist <matilda.dahlqvist@Matilda-Dahlqvist.local>
2019-02-15 18:14:09 +0100

    application-input-fields-and-button

--- client/src/containers/application/ApplicationView.jsx
+++ client/src/containers/application/ApplicationView.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
 import './application.scss'
 
+function InputField (props) {
+  return <div>
+    <label>
+      {props.label}
+    </label>
+    <input onChange={props.valueChanged} type="text" value={props.value} ></input>
+  </div>
+}
 export default function ApplicationView(props) {
-  return <div className="application-view">hej</div>
+console.log(props)
+  return <div className="application-view">
+    <InputField valueChanged={props.nameChanged} 
+        label="Namn" 
+        value={props.name}/>
+    <InputField valueChanged={props.ageChanged} 
+        label="Ålder" 
+        value={props.age}/>
+    <InputField valueChanged={props.amountChanged}
+        label="Belopp"
+        value={props.amount}/>
+    <InputField valueChanged={props.durationChanged}
+        label="Hur länge?"
+        value={props.duration}/>
+    <InputField valueChanged={props.reasonChanged}
+        label="Orsak"
+        value={props.reason}/>
+        <button onClick={props.onClick}>
+       Ansök</button>
+   
+  </div>
 }
